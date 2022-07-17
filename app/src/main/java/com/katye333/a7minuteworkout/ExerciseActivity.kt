a6753d74910147bcc8b0a1251c4c8618e3573212
Katye Stevens <katye333@gmail.com>
2022-07-17 14:05:13 -0400

    feat: Use countdown timers to display the seconds remaining

--- app/src/main/java/com/katye333/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/katye333/a7minuteworkout/ExerciseActivity.kt
@@ -2,11 +2,15 @@ package com.katye333.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import com.katye333.a7minuteworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
     private var binding : ActivityExerciseBinding? = null
+    private var restTimer : CountDownTimer? = null
+    private var restProgress : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +28,44 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView() {
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar() {
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object: CountDownTimer(10000, 1000) {
+            // every second run this code
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString() //
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity, "Start the exercise", Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+
+        binding = null
     }
 }
