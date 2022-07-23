a781dc9c68d82be45ed0aceee38b8107d9fbed15
Katye Stevens <katye333@gmail.com>
2022-07-23 16:30:28 -0400

    feat: Display data in the workout history recycler view

--- app/src/main/java/com/katye333/a7minuteworkout/HistoryActivity.kt
+++ app/src/main/java/com/katye333/a7minuteworkout/HistoryActivity.kt
@@ -3,7 +3,9 @@ package com.katye333.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.katye333.a7minuteworkout.databinding.ActivityHistoryBinding
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -35,8 +37,25 @@ class HistoryActivity : AppCompatActivity() {
     private fun getAllCompletedDates(historyDao: HistoryDao) {
         lifecycleScope.launch {
             historyDao.fetchAllDates().collect { allCompletedDatesList ->
-                for (i in allCompletedDatesList) {
-                    Log.e("Date: ", "" + i)
+                if (allCompletedDatesList.isNotEmpty()) {
+                    binding?.tvHistory?.visibility = View.VISIBLE
+                    binding?.rvHistory?.visibility = View.VISIBLE
+                    binding?.tvNoDataAvailable?.visibility = View.INVISIBLE
+
+                    binding?.rvHistory?.layoutManager = LinearLayoutManager(this@HistoryActivity)
+
+                    val dates = ArrayList<String>()
+                    for (date in allCompletedDatesList) {
+                        dates.add(date.date)
+                    }
+
+                    val historyAdapter = HistoryAdapter(dates)
+                    binding?.rvHistory?.adapter = historyAdapter
+                }
+                else {
+                    binding?.tvHistory?.visibility = View.GONE
+                    binding?.rvHistory?.visibility = View.GONE
+                    binding?.tvNoDataAvailable?.visibility = View.VISIBLE
                 }
             }
         }
